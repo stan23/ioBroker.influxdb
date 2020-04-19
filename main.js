78de1cea621afe6a784e0e0921424def06438e5d
Marco <marco-github@mageis.de>
2020-04-19 21:47:39 +0200

    ignore storeState is callback indicates an error

--- main.js
+++ main.js
@@ -1290,9 +1290,11 @@ function query(adapter, msg) {
 
 function storeState(adapter, msg) {
     if (!msg.message || !msg.message.id || !msg.message.state) {
-         // if this is already an successful callback then do not send back another message
+        // if this is already an successful callback then do not send back another message
         if (msg.success && msg.success === true) {
             adapter.log.debug('storeState called with invalid data, but as part of a callback that indicates success.');
+        } else if (msg.error) {
+            adapter.log.debug('storeState called with invalid data, but as part of a callback that indicates an error');
         } else {
             adapter.log.error('storeState called with invalid data');
             adapter.sendTo(msg.from, msg.command, {
